9eea5ba92ecd4c1a1c2d859c664c1fe1cb0919b8
Razvan Lazea <rlazea_ext@icontrol.com>
2016-11-01 16:29:09 +0200

    For production

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :branch, "abranch"
 # set :format, :airbrussh
 set :user, "reactor"
 set :use_sudo, false
-set :rails_env, "development"
+set :rails_env, "production"
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
